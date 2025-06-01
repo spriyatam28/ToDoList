047e669ca78b7792e812db8248589c41c9daec19
Pritam <180375336+spriyatam28@users.noreply.github.com>
2025-06-01 11:00:30 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Pritam on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
